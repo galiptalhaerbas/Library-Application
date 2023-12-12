f0ddf9cc27a5dfb4e4fa61d1174698c3ee39a1e8
galiperbas <galipterbas@gmail.com>
2023-12-12 21:52:48 +0300

    some developments

--- main.py
+++ main.py
@@ -2,44 +2,88 @@ def main_menu() -> None:
     print("\nLibrary Application Menu")
     print("1. List all the books in the library")
     print("2. List all the books that are checked out")
-    print("3. Delete a book")
-    print("4. Search a book by ISBN number")
-    print("5. Search a book by name")
-    print("6. Check out a book to a student")
-    print("7. List all the students")
-    print("8. Exit")
+    print("3. Add a new book")
+    print("4. Delete a book")
+    print("5. Search a book by ISBN number")
+    print("6. Search a book by name")
+    print("7. Check out a book to a student")
+    print("8. List all the students")
+    print("9. Exit")
+
 
 def list_books():
-    pass
+    with open("books.txt", "r") as file:
+        file.read()
+
+
 def list_checked_books():
     pass
+
+
+def add_book():
+    isbn_number = input("What is isbn number: ").strip()
+    book_name = input("What is name: ").strip()
+    author_name = input("What is author name: ").strip()
+    checked = "F"
+
+    with open("books.txt", "a") as file:
+        file.write(f"{isbn_number},{book_name},{author_name},{checked}")
+
+
 def delete_book():
     pass
+
+
 def search_by_isbn():
-    pass
+    isbn_number = input("What is isbn number: ").strip()
+    with open("books.txt", "r") as file:
+        line = file.readline()
+        while line:
+            items = line.split(",")
+            if items[0] == isbn_number:
+                print(line)
+                break
+            line = file.readline()
+
+
 def search_by_name():
-    pass
+    book_name = input("What is name: ").strip()
+    with open("books.txt", "r") as file:
+        line = file.readline()
+        while line:
+            items = line.split(",")
+            if book_name in items[1]:
+                print(line)
+                break
+            line = file.readline()
+
+
 def check_out_book():
     pass
+
+
 def list_students():
-    pass
+    with open("students.txt", "r") as file:
+        file.read()
+
 
 def main_loop():
-    func_list = [list_books, list_checked_books, delete_book,
+    func_list = [list_books, list_checked_books, add_book, delete_book,
                  search_by_isbn, search_by_name, check_out_book, list_students]
     while True:
         main_menu()
         option = input("\nYour option (1/8): ")
 
-        try: 
+        try:
             option = int(option)
-            if option == 8:
+            if option == 9:
                 break
-            if 1 <= option <=7:
-                func_list[option-1]()
+            if 1 <= option <= 8:
+                func_list[option - 1]()
             else:
                 print("Invalid option, try again!")
         except:
             print("Invalid option, try again!")
+
 
 main_loop()
